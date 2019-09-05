e9d1abee01a04850e0ad7dfe266b9d3bc4b62e0e
KodyJKing <kody.j.king@gmail.com>
2019-09-05 01:47:20 -0700

    Added Liberate index generator.

---
+++ src/Index.ts
@@ -0,0 +1,29 @@
+import * as _0 from "./assets"
+import * as _1 from "./audio"
+import * as _2 from "./boundingBoxUtils"
+import * as _3 from "./Canvas"
+import * as _4 from "./common"
+import * as _5 from "./gameLoop"
+import * as _6 from "./GameObject"
+import * as _7 from "./IBoundingBox"
+import * as _8 from "./IGame"
+import * as _9 from "./Index"
+import * as _10 from "./Input"
+import * as _11 from "./Sprite"
+import * as _12 from "./Vector"
+
+export default {
+    _0,
+    _1,
+    _2,
+    _3,
+    _4,
+    _5,
+    _6,
+    _7,
+    _8,
+    _9,
+    _10,
+    _11,
+    _12
+}
