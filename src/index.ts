54fb3a16d5d87667f9fa85a9fea1a175a14c6a11
Kody King <kody.j.king@gmail.com>
2019-09-08 18:05:17 -0700

    added index back with proper casing

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import * as _0 from "./assets"
+import * as _1 from "./audio"
+import * as _2 from "./collision/collision"
+import * as _3 from "./collision/IBoundingBox"
+import * as _4 from "./gameobject/Component"
+import * as _5 from "./gameobject/GameObject"
+import * as _6 from "./gameobject/IComponent"
+import * as _7 from "./graphics/Canvas"
+import * as _8 from "./graphics/Color"
+import * as _9 from "./graphics/Sprite"
+import * as _10 from "./graphics/SpriteSheet"
+import * as _11 from "./IGame"
+import * as _12 from "./Input"
+import * as _13 from "./math/GMath"
+import * as _14 from "./math/Matrix"
+import * as _15 from "./math/MutableVector"
+import * as _16 from "./math/Transform"
+import * as _17 from "./math/Vector"
+import * as _18 from "./util"
+
+export default {
+    _0,
+    _1,
+    _2,
+    _3,
+    _4,
+    _5,
+    _6,
+    _7,
+    _8,
+    _9,
+    _10,
+    _11,
+    _12,
+    _13,
+    _14,
+    _15,
+    _16,
+    _17,
+    _18
+}
